d62e62da6029e3a79817f6e54194e6bb18c2ce0f
RAJAT SEMWAL <rajatsemwal17@gmail.com>
2025-08-13 23:39:04 +0530

    added a js-library for background and worked more on JavaScript

---
+++ JS/about.js
@@ -0,0 +1,17 @@
+import {products} from './data/products.js';
+
+let productHTML = '';
+
+products.forEach((product) => {
+  productHTML += `
+    <div class="box1">
+      <img src=${product.image} class="image">
+      <div class="heading">${product.title}</div>
+      <div class="cnt">
+        ${product.para}
+      </div>
+    </div>
+  `
+})
+console.log(productHTML);
+document.querySelector('.content').innerHTML = productHTML;
---
+++ JS/data/products.js
@@ -0,0 +1,18 @@
+export const products = [{
+  title: 'Student Services',
+  image:'"../images/student.png"',
+  para : 'Manage attendance, ID cards, and academic info in one place.'
+},{
+  title: 'Notices & Announcements',
+  image:'"../images/announcement.png"',
+  para : 'Get instant updates on notices, circulars, and announcements.'
+},{
+  title: 'Faculty Directory',
+  image:'"../images/faculty.png"',
+  para : 'Find faculty contact details, office hours, and specialization areas.'
+},{
+  title: 'Course Materials',
+  image:'"../images/courses.png"',
+  para : 'Access lecture notes, assignments, and learning resources in one place.'
+}
+]
