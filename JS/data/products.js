9b62828dff50e7508134eeacab5198f0dc9ffa65
RS17 🤷‍♂️ <rajat0673.becse24@chitkara.edu.in>
2025-08-14 07:35:45 +0530

    Update products.js

--- JS/data/products.js
+++ JS/data/products.js
@@ -4,16 +4,17 @@ export const products = [{
   para : 'Manage attendance, ID cards, and academic info in one place.'
 },{
   title: 'Notices & Announcements',
-  image:'"../images/announcement.png"',
+  image:'images/announcement.png',
   para : 'Get instant updates on notices, circulars, and announcements.'
 },{
   title: 'Faculty Directory',
-  image:'"../images/faculty.png"',
+  image:'images/faculty.png',
   para : 'Find faculty contact details, office hours, and specialization areas.'
 },{
   title: 'Course Materials',
-  image:'"../images/courses.png"',
+  image:'images/courses.png',
   para : 'Access lecture notes, assignments, and learning resources in one place.'
 }
 ]
+
 
