d91b9759d50af6c5f64db5706780fe60f0f0faff
RAJAT SEMWAL <rajatsemwal17@gmail.com>
2025-10-27 19:40:57 +0530

    more changes added

---
+++ JS/contact.js
@@ -0,0 +1,7 @@
+let contactUs = document.querySelector(".sendMessage");
+contactUs.addEventListener('click', ()=> {
+  alert('Message Sent!!');
+  window.addEventListener('load', () => {
+    console.log(2);
+});
+})
